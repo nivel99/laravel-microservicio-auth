c84e13b202e8d2ee0a59742ad6220cfe0e36323c
inteligenciafutura <inteligenciafuturatv@gmail.com>
2023-11-29 15:21:52 -0500

    campos de documento telefono y tipo de documento

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,9 @@ class User extends Authenticatable
         'email',
         'password',
         'rol',
+        'telefono',
+        'tipodocumento',
+        'numerodocumento'
     ];
 
     /**
